974263fc41ca69ace62379e7ad5857228555c34d
supriya2246 <supriya4622@gmail.com>
2022-12-01 19:01:56 +0530

    file

---
+++ LoopStmnt.java
@@ -0,0 +1,8 @@
+class ForLoop{
+	public static void main(String[] args){
+		int sum=0;
+		for(int i=1; i<=100; i++){
+			sum=sum+i;
+		}
+		System.out.println("Display Sum Of 1 to 10 No's:"+ sum);
+}}//5050
---
+++ LoopStmnt1.java
@@ -0,0 +1,18 @@
+class ForLoop{
+	public static void main(String[] args){
+		int no=0;
+		for(int i=1; i<+10; i++){
+			System.out.println(i);
+		}
+	}
+}/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+*/
